38d0c18e8dbb974c72edd0eb90fa7e3fac6503af
oesi <oesi@technikum-wien.at>
2015-12-15 17:10:07 +0100

    Code Parameter korrigiert

--- cis/qrcode.php
+++ cis/qrcode.php
@@ -107,7 +107,7 @@ foreach($codes_obj->result as $code)
 	$files[]=$filename;
 
 	// QRCode ertellen und speichern
-	QRcode::png($url_detail.'?c='.$code->code, $filename);
+	QRcode::png($url_detail.'?code='.$code->code, $filename);
 
 	// QRCode zu Dokument hinzufuegen
 	$doc->addImage($filename, $code->lvevaluierung_code_id.'.png', 'image/png');
