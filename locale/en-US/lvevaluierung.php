7f4b59e880d0bcce60dee345a3e758b68b310db1
kindlm <kindlm@technikum-wien.at>
2016-03-09 14:27:46 +0100

    Englische Phrasen eingepflegt

---
+++ locale/en-US/lvevaluierung.php
@@ -0,0 +1,65 @@
+<?php
+$this->phrasen['lvevaluierung/lvevaluierung']='Course evaluation';
+$this->phrasen['lvevaluierung/welcome']='Course evaluation';
+$this->phrasen['lvevaluierung/code']='Code';
+$this->phrasen['lvevaluierung/einleitungstext']='Please enter your code to start the evaluation';
+$this->phrasen['lvevaluierung/codeAbgelaufen']='This code has already been used.';
+$this->phrasen['lvevaluierung/codeFalsch']='The code entered is invalid';
+$this->phrasen['lvevaluierung/nichtoffen']='This survey is currently not open';
+$this->phrasen['lvevaluierung/keineAngabe']='not applicable';
+$this->phrasen['lvevaluierung/lvbezeichnung']='Course designation';
+$this->phrasen['lvevaluierung/lvleitung']='Course teacher';
+$this->phrasen['lvevaluierung/organisationsform']='Organizational form';
+$this->phrasen['lvevaluierung/lvtyp']='Course type';
+$this->phrasen['lvevaluierung/ects']='ECTS';
+$this->phrasen['lvevaluierung/ausbildungssemester']='Educational semester';
+$this->phrasen['lvevaluierung/anzahlstudierende']='Number of students';
+$this->phrasen['lvevaluierung/danke']='The survey has been successfully completed. Thank you for your participation!';
+$this->phrasen['lvevaluierung/startzeit']='Start of the evaluation';
+$this->phrasen['lvevaluierung/endezeit']='End of the evaluation';
+$this->phrasen['lvevaluierung/dauer']='Processing time';
+$this->phrasen['lvevaluierung/evaluierungBearbeiten']='Process evaluation';
+$this->phrasen['lvevaluierung/evaluierungAnlegen']='Create evaluation';
+$this->phrasen['lvevaluierung/zeitinfo']='The course evaluation will take place from %s to %s.';
+$this->phrasen['lvevaluierung/restzeit']='Time remaining:';
+$this->phrasen['lvevaluierung/auswertung']='Analysis';
+$this->phrasen['lvevaluierung/keineEvaluierungAngelegt']='At this time no evaluation for this course has been created. Please complete the following form to create an evaluation';
+$this->phrasen['lvevaluierung/CodeListeErstellen']='Create code list';
+$this->phrasen['lvevaluierung/Auswertung']='Display analysis';
+$this->phrasen['lvevaluierung/abgeschlossen']='Completed';
+$this->phrasen['lvevaluierung/durchschnittszeit']='Average duration';
+$this->phrasen['lvevaluierung/anzahlausgegeben']='Codes issued: ';
+$this->phrasen['lvevaluierung/endeGroesserStart']='The end of the evaluation may not be before the start';
+$this->phrasen['lvevaluierung/bitteGueltigesDatumEingeben']='The date for the start and end may not be left blank and must be entered in the DD.MM.YYYY format';
+
+$this->phrasen['lvevaluierung/evaluierunganlegen']='Specify evaluation period';
+$this->phrasen['lvevaluierung/evaluierunganlegenInfotext']='Please specify here the period in which the course evaluation is to take place. Access to the evaluation is only available to students in this period. You may correct the period at any time, as long as the evaluation has not yet been carried out.';
+$this->phrasen['lvevaluierung/dauerInfotext']='The processing time defines how much time the students have to complete the evaluation after it has been started. We recommend that you keep to the preset 10 minutes. However, the length of time can be adapted.';
+
+$this->phrasen['lvevaluierung/codesErstellen']='Print the list of the access codes';
+$this->phrasen['lvevaluierung/codesErstellenInfotext']='Click on the link to download a PDF with codes. This can take a few seconds with a large group. Then print the document.';
+
+$this->phrasen['lvevaluierung/evaluierungDruchfuehren']='Carry out the evaluation';
+$this->phrasen['lvevaluierung/evaluierungDruchfuehrenInfotext']='Carry out the course evaluation within the specified time period.';
+
+$this->phrasen['lvevaluierung/codesAusgegeben']='Access codes issued';
+$this->phrasen['lvevaluierung/codesAusgegebenInfotext']='Please enter here the number of codes that were in fact used in the evaluation and shown on the form.';
+$this->phrasen['lvevaluierung/codesAusgegebenAnzahl']='Number of codes used';
+$this->phrasen['lvevaluierung/mehrCodesAusgegebenAlsStudierende']='The number of codes used may not exceed the number of students in the course.';
+
+$this->phrasen['lvevaluierung/auswertungAnzeigen']='Analysis';
+$this->phrasen['lvevaluierung/auswertungAnzeigenInfotext']='Click here to display the results of the course evaluation.';
+
+$this->phrasen['lvevaluierung/selbstevaluierung']='Self-evaluation';
+$this->phrasen['lvevaluierung/selbstevaluierungInfotext']='Complete the following fields in the self-evaluation of the course. The data will also be sent to the head of the degree program and the head of department.';
+
+$this->phrasen['lvevaluierung/speichernundabschicken']='Save and send';
+$this->phrasen['lvevaluierung/confirmEvaluierungAbschicken']='The text boxes are locked and the self-evaluation has been sent. Do you want to continue?';
+
+$this->phrasen['lvevaluierung/XhatEineEvaluierungDurchgefuehrt']='%s has carried out an evaluation of the course:';
+$this->phrasen['lvevaluierung/selbstevaluierungGruppe']='1. How do you rate the group of students (e.g. as regards prior knowledge, willingness to learn, commitment, participation…)?';
+$this->phrasen['lvevaluierung/selbstevaluierungPersoenlich']='2. Personal assessment of the course from the background of the results of the evaluation:';
+$this->phrasen['lvevaluierung/selbstevaluierungGeplanteEntwicklung']='3. Measures planned to develop the course:';
+$this->phrasen['lvevaluierung/selbstevaluierungWeiterbildung']='4. From the measures planned to develop the course does a need arise for further training in university didactics?';
+$this->phrasen['lvevaluierung/uebersicht']='Overview';
+?>
