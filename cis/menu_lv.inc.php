4058de86f4ca9e125e1773727287f52e6b193947
Andreas Österreicher <oesi@technikum-wien.at>
2023-11-17 12:32:40 +0100

    Pfad korrigiert für Einbindung der Klasse da in manchen Setups sonst falsche Versionen eingebunden werden

--- cis/menu_lv.inc.php
+++ cis/menu_lv.inc.php
@@ -16,8 +16,8 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
  *
  */
-require_once(DOC_ROOT . 'addons/lvevaluierung/include/lvevaluierung.class.php');
-require_once(DOC_ROOT . 'addons/lvevaluierung/lvevaluierung.config.inc.php');
+require_once(dirname(__FILE__) . '/../include/lvevaluierung.class.php');
+require_once(dirname(__FILE__) . '/../lvevaluierung.config.inc.php');
 
 /**
  * Hinzufuegen von neuen Menuepunkten bei CIS Lehrveranstaltungen
